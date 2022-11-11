05624645ba244e5160c7b400e69fa619610d7bc8
edson <j.edson.da.silva.s@gmail.com>
2022-11-11 15:35:25 -0300

    configuração da autenticaç

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,6 @@
 import { Center, Text, Icon } from 'native-base'
 import { Fontisto } from '@expo/vector-icons'
-import { useAuth } from '../hookes/useAuth'
+import { useAuth } from '../hooks/useAuth'
 
 import Logo from '../assets/logo.svg'
 import { Button } from '../components/Button'
